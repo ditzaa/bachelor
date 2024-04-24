f17eef730b4532aa892c00718e198d6687652105
Mitu Adrian <adrianmitu83@gmail.com>
2024-04-24 13:40:52 +0300

    Added basic LoginPg

--- frontend/src/Pages/LoginForm.jsx
+++ frontend/src/Pages/LoginForm.jsx
@@ -1,38 +1,42 @@
 import "./Login.css";
+import Navbar from "../Components/Home/Navbar";
 import { FaUser, FaLock } from "react-icons/fa";
 
 const LoginForm = () => {
   return (
-    <div className="wrapper">
-      <h1>Login</h1>
-      <form action="">
-        <div className="input-box">
-          <input type="text" placeholder="Username" required />
-          <FaUser className="icon" />
-        </div>
-        <div className="input-box">
-          <input type="password" placeholder="Password" required />
-          <FaLock className="icon" />
-        </div>
+    <>
+      {/* <Navbar></Navbar> */}
+      <div className="wrapper">
+        <h1>Login</h1>
+        <form action="">
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <FaLock className="icon" />
+          </div>
 
-        <div className="remember-forgot">
-          <label>
-            {" "}
-            <input type="checkbox" />
-            Remember me
-          </label>
-          <a href="#">Forgot password?</a>
-        </div>
+          <div className="remember-forgot">
+            <label>
+              {" "}
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <a href="#">Forgot password?</a>
+          </div>
 
-        <button type="submit">Login</button>
+          <button type="submit">Login</button>
 
-        <div className="register-link">
-          <p>
-            {"Don't have an account?"} <a href="#">Register</a>
-          </p>
-        </div>
-      </form>
-    </div>
+          <div className="register-link">
+            <p>
+              {"Don't have an account?"} <a href="#">Register</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
