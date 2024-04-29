09be5c66e58a2015267b8d8c5cf40d0ed46fda1b
Mitu Adrian <adrianmitu83@gmail.com>
2024-04-29 16:21:41 +0300

    Styled a bit the login form

--- frontend/src/Pages/LoginForm.jsx
+++ frontend/src/Pages/LoginForm.jsx
@@ -1,40 +1,49 @@
 import "./Login.css";
 import Navbar from "../Components/Home/Navbar";
+//import BannerBackground from "../../assets/home-banner-background.png";
+import BannerBackground from "../assets/home-banner-background.png";
 import { FaUser, FaLock } from "react-icons/fa";
 
 const LoginForm = () => {
   return (
     <>
-      {/* <Navbar></Navbar> */}
-      <div className="wrapper">
-        <h1>Login</h1>
-        <form action="">
-          <div className="input-box">
-            <input type="text" placeholder="Username" required />
-            <FaUser className="icon" />
-          </div>
-          <div className="input-box">
-            <input type="password" placeholder="Password" required />
-            <FaLock className="icon" />
-          </div>
+      <Navbar></Navbar>
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+      </div>
+      <div className="login-container">
+        <div className="wrapper">
+          <h1>Authenticate</h1>
+          <form action="">
+            <div className="input-box">
+              <input type="text" placeholder="Username" required />
+              <FaUser className="icon" />
+            </div>
+            <div className="input-box">
+              <input type="password" placeholder="Password" required />
+              <FaLock className="icon" />
+            </div>
 
-          <div className="remember-forgot">
-            <label>
-              {" "}
-              <input type="checkbox" />
-              Remember me
-            </label>
-            <a href="#">Forgot password?</a>
-          </div>
+            <div className="remember-forgot">
+              <label>
+                {" "}
+                <input type="checkbox" />
+                Remember me
+              </label>
+              <a href="#">Forgot password?</a>
+            </div>
 
-          <button type="submit">Login</button>
+            <button type="submit">Login</button>
 
-          <div className="register-link">
-            <p>
-              {"Don't have an account?"} <a href="#">Register</a>
-            </p>
-          </div>
-        </form>
+            <div className="register-link">
+              <p>
+                {"Don't have an account?"} <a href="#">Register</a>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
