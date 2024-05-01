81f311d37d9001619e4820e06e0e6a4db2797536
Mitu Adrian <adrianmitu83@gmail.com>
2024-05-01 23:43:22 +0300

    Made basic login route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -21,7 +21,29 @@ const controller = {
 
       const user = await UserDb.create(payload);
       res.status(201).send(user);
-    } catch {
+    } catch (error) {
+      res.status(500).send("Server error!" + error.message);
+    }
+  },
+
+  loginUser: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+
+      const user = await UserDb.findOne({ where: { username: username } });
+
+      if (!user) {
+        return res.json({ error: "User Doesn't Exist" });
+      }
+
+      bcrypt.compare(password, user.password).then((match) => {
+        if (!match) {
+          return res.json({ error: "Wrong Username And Password Combination" });
+        }
+
+        res.json("YOU LOGGED IN!!!");
+      });
+    } catch (error) {
       res.status(500).send("Server error!" + error.message);
     }
   },
